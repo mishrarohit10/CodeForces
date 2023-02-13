3803858d509b1295712a348770e7a394fb82dd14
Rohit Mishra <89183101+mishrarohit10@users.noreply.github.com>
2023-02-13 21:57:01 +0530

    Add files via upload

---
+++ A_Maximum_GCD.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class A_Maximum_GCD {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t-- >0) {
+            int n = in.nextInt();
+            System.out.println(n/2);
+        }
+    }
+}
