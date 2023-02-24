21e279bc3d558d2426a7adf88a3f3f14034eecdf
Rohit Mishra <89183101+mishrarohit10@users.noreply.github.com>
2023-02-25 03:31:05 +0530

    Add files via upload

---
+++ template2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Codeforces {
+    static PrintWriter out = new PrintWriter(System.out);
+    static Scanner cs = new Scanner(System.in);
+    
+    public static void main(String[] args) throws Exception {
+        int t = cs.nextInt();
+        while(t-- > 0) {
+            
+        }
+        out.close();
+    }
+}
