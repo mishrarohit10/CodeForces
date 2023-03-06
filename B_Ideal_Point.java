50afddc22fdacbf5585855ff378703329e946723
Rohit Mishra <89183101+mishrarohit10@users.noreply.github.com>
2023-03-07 02:19:52 +0530

    Add files via upload

--- B_Ideal_Point.java
+++ B_Ideal_Point.java
@@ -6,24 +6,17 @@ public class B_Ideal_Point {
         while(t-->0) {
             int n = in.nextInt();
             int k = in.nextInt();
-            int start = 0;
-            int end = 0;
-            for(int i=0;i<n;i++) {
-                int l = in.nextInt();
-                int r = in.nextInt();
-                if(l==k) {
-                    start++;
-                }
-                if(r==k) {
-                    end++;
+            int L =0 , R = 55;
+            while(n-->0) {
+                int l,r;
+                 l = in.nextInt();
+                 r = in.nextInt();
+                if(l<=k && k<=r) {
+                    L = Math.max(L,l);
+                    R = Math.min(R,r);
                 }
             }
-            if(start>0 && end>0) {
-                System.out.println("YES");
-            }
-            else {
-                System.out.println("NO");
-            }
+            System.out.println(L == R ? "YES\n" : "NO\n");
         }
     }
 }
